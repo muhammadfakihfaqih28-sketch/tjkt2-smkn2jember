abf7967f8da0e245af676197dbd5f4405f1fb949
Fakih27 <muhammadfakihfaqih28@gmail.com@gmail.com>
2026-08-08 12:41:26 +0700

    Bypass Vercel env issue via vercel.json

--- api/index.php
+++ api/index.php
@@ -1,3 +1,14 @@
 <?php
 
+// Paksa Laravel memakai folder /tmp bawaan Vercel untuk cache & storage
+$storagePath = '/tmp/storage';
+if (!is_dir($storagePath)) {
+    mkdir($storagePath . '/framework/views', 0755, true);
+    mkdir($storagePath . '/framework/cache/data', 0755, true);
+    mkdir($storagePath . '/framework/sessions', 0755, true);
+}
+
+putenv("APP_STORAGE_PATH={$storagePath}");
+putenv("VIEW_COMPILED_PATH={$storagePath}/framework/views");
+
 require __DIR__ . '/../public/index.php';
